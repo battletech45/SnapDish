720dbd359a071c5437477fb1781ea9d37f3511e8
AltayTaneri <taneri862@gmail.com>
2024-01-15 15:56:48 +0300

    fixed height of payment

--- src/components/payment/payment.jsx
+++ src/components/payment/payment.jsx
@@ -5,8 +5,8 @@ import Order from "../order/order";
 
 const Payment = () => {
   return (
-    <section className="flex grow">
-      <div className="grow flex flex-col h-screen ml-24 bg-[#252836] p-8">
+    <section className="flex grow h-screen">
+      <div className="grow flex flex-col ml-24 bg-[#252836] p-8">
         <PaymentHeader />
         <PaymentBody />
       </div>
