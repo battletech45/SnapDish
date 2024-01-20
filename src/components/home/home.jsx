db612b3c9a379e4a271dd1af0783fcab80e4d0ad
AltayTaneri <taneri862@gmail.com>
2024-01-20 14:36:14 +0300

    updated tailwind

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -6,7 +6,7 @@ import Order from "../order/order";
 const Home = () => {
   return (
     <section className="flex grow">
-      <section className="grow flex flex-col ml-24 h-screen overflow-y-scroll">
+      <section className="grow flex flex-col ml-20 md:ml-24 h-screen overflow-y-scroll">
         <HomeHeader />
         <HomeBody />
       </section>
